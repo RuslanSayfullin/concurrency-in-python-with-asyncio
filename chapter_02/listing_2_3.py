017842d8aff02534531c9b59f82714c32dc4617f
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-04-26 15:22:45 +0500

    Основы синтаксиса async await и сопрограмм.

---
+++ chapter_02/listing_2_3.py
@@ -0,0 +1,11 @@
+# asyncio.run, которую можно использовать для запуска нашей сопрограммы.
+import asyncio
+
+
+async def coroutine_add_one(number: int) -> int:
+    return number + 1
+
+
+result = asyncio.run(coroutine_add_one(1))
+
+print(result)
