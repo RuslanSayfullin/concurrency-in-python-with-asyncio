e4e9b695014f7a4ef6a9d9c6e0d18849e2d2cb4e
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-03-18 10:41:39 +0500

    Исполнитель пула процессов в сочетании с asyncio

---
+++ chapter_06/listing_6_5.py
@@ -0,0 +1,31 @@
+# испонитель пула процессов в сочетаний с asyncio
+import asyncio
+from asyncio.events import AbstractEventLoop
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from typing import list
+
+def count(count_to: int) -> int:
+    counter = 0
+    while counter < count_to:
+        counter += 1
+        return counter
+
+async def main():
+    with ProcessPoolExecutor() as process_pool:
+        loop: AbstractEventLoop = asyncio.get_running_loop()
+        nums = [1, 3, 5, 22, 100000000]
+        calls: List[partial[int]] = [partial(count, num) for num in nums]
+        call_coros = []
+
+        for call in calls:
+            call_coros.append(loop.run_in_executor(process_pool, call))
+
+        result = await asyncio.gather(*call_coros)  # ждём получения результатов
+
+        for result in results:
+            print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
